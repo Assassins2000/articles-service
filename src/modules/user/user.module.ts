e3ed61c1aa879a170dded25e1e50da8fafdd277d
Assassins2000 <css.ctt5@gmail.com>
2025-02-06 02:21:17 +0800

    Configurate module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostgresModule } from '../postgres';
+import { AccountEntryController } from './user.controller';
+import { UserService } from './user.service';
+import { UserData } from './user.data';
 
 @Module({
   imports: [PostgresModule],
-  controllers: [],
-  providers: [],
+  controllers: [AccountEntryController],
+  providers: [UserService, UserData],
 })
 export class UserModule {}
